075c79868d686865aef342082d9c74cdb762b7c7
Idel Pagan <idelpagan@gmail.com>
2024-07-20 19:02:28 -0600

    changed trading keys startup sequence

--- trading-keys.bat
+++ trading-keys.bat
@@ -1,7 +1,13 @@
 @echo off
 
 rem Change directory to your specific folder
-cd /d "C:\Documents\trading-keys"
+cd /d "C:\Documents\trading-keys-next"
+
+rem Run npm run dev
+npm run dev
+
+rem Change directory to your specific folder
+cd /d "C:\Documents\trading-keys-webdriverIO"
 
 rem Run npm run dev
 npm run dev
